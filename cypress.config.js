e7516dc6e9d7f7f22be1dd6851428094aaeac86f
Trivia <triviaside@protonmail.ch>
2023-02-13 11:06:54 +0700

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require("cypress");
+const preprocessor = require('@badeball/cypress-cucumber-preprocessor');
+const browserify = require('@badeball/cypress-cucumber-preprocessor/browserify');
+
+async function setupNodeEvents(on, config) {
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+  on("file:preprocessor", browserify.default(config));
+  return config;
+}
+
+module.exports = defineConfig({
+  video: false,
+  defaultCommandTimeout: 5000,
+  pageLoadTimeout: 2000, e2e: {
+    specPattern: "**/*.feature",
+    supportFile: false,
+    setupNodeEvents(on, config) {
+    },
+  },
+});
